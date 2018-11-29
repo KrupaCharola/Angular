75c30c1b0059ffdd986b24be200b9567fd6d5a30
KrupaCharola <krupacharola1995@gmail.com>
2018-11-29 23:59:04 +0530

    Rendering Flows Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,58 @@
 import { Component } from '@angular/core';
 import { template } from '@angular/core/src/render3';
 import { bloomHashBitOrFactory } from '@angular/core/src/render3/di';
-import { truncate } from 'fs';
 
+interface Foody {
+  name: string;
+  taste: string;
+  rating: number;
+  isCold: boolean;
+  date?: number;
+  price: number;
+  choices?:Array<choicesForFood>
+}
+
+interface choicesForFood{
+  name:string;
+  price:number;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-
+  styleUrls: ['./app.component.css', './app.component.scss'],
 })
 export class AppComponent {
+
   name: string = ""
-  title:string="Krupa's PC"
-  guest:string="Hellow Abhi!" // why whitespace does not effect in here?
-  nameChangedEvent(value:string) {
-    this.name=value
+  title: string = "Krupa's PC"
+  foodChoice = new Array<Foody>({
+    name: "Pizza",
+    taste: "spicy",
+    rating: 10,
+    isCold: false,
+    date: 9898767898767, 
+    price: 500,
+    choices:[{name:"margeritta",price:500}]
+  },
+    {
+      name: "KajuKatri",
+      taste: "sweet",
+      rating: 7,
+      isCold: false,
+      date: null,
+      price: 5000
+    }, {
+      name: "Balaji Waffers",
+      taste: "salty",
+      rating: 7,
+      isCold: true,
+      date: null,
+      price: 540
+    }
+  );
+
+  nameChangedEvent(value: string) {
+    this.name = value
   }
+
 }
